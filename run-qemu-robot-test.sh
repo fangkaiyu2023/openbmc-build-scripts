9c224a5a7df2cf662c2f21387800a4a1953b227f
fangkaiyu2023 <130332147+fangkaiyu2023@users.noreply.github.com>
2023-05-22 18:12:48 +0800

    Update run-qemu-robot-test.sh

--- run-qemu-robot-test.sh
+++ run-qemu-robot-test.sh
@@ -152,6 +152,7 @@ if [[ ${LAUNCH} == "local" ]]; then
         attempt=$(( attempt - 1 ))
         echo "Waiting for qemu to get to standby (attempt: $attempt)..."
         echo ${obmc_qemu_docker}
+        docker attach ${obmc_qemu_docker}
         result=$(docker logs "$obmc_qemu_docker")
         if grep -q 'OPENBMC-READY' <<< "$result" ; then
             echo "QEMU is ready!"
